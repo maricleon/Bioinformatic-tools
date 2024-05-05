df06a4ef463d581f5d9824bd90eb0cd06ee263d0
Leon Maric <101092069+maricleon@users.noreply.github.com>
2024-05-05 20:59:06 +0200

    Update ONT-WGS.sh

--- ONT-WGS.sh
+++ ONT-WGS.sh
@@ -3,11 +3,13 @@
 #cat all files in each directory and name it after directory
 for f in *AMR*; do cat "$f"/* > "${f}.cat.fastq.gz"; done
 
+######################################## P L O T ########################################
+
 for f in *.fastq.gz; do     
 	NanoPlot -t 8 --fastq $f --no_static --no_supplementary --tsv_stats --outdir "${f%.cat.fastq.gz}.plot"; 
 done
 
-#ta zanka preimenuje datoteke NanoStats.txt v vsaki mapi tako, da vsaki datoteki dodeli ime, kot jo ima mapa
+#this loop rename all NanoStats.txt in each file after a folders name
 for p in */NanoStats.txt; do [ -f "$p" ] && mv -i "$p" "${p%/*}/${p/\//.}"; done
 
 mkdir stats
@@ -18,13 +20,13 @@ awk -i inplace -v ORS='\r\n' 'FNR==1{print FILENAME}1' ./stats/*
 
 paste -s ./stats/* > ./stats/merged.tsv 
 
-######################################### F L T R ########################################
+#################################### F L T R ########################################
 for f in *cat.fastq.gz; do
     gunzip -c $f | NanoFilt -q 10 -l 5000 | gzip > "${f%.cat*}.filtered.fastq.gz";
 done
 
 
-################################## PONOVI NANOPLOT NA FILTRIRANIH SEKVENCAH ###################################
+################################## P L O T   A G A I N ###################################
 
 for f in *.filtered.fastq.gz; do     
 	NanoPlot -t 8 --fastq $f --no_static --no_supplementary --tsv_stats --outdir "${f%.fastq.gz}.plot"; 
@@ -40,7 +42,7 @@ awk -i inplace -v ORS='\r\n' 'FNR==1{print FILENAME}1' ./stats2/*
 
 paste -s ./stats2/* > ./stats2/merged2.tsv 
 
-###################### UREDIM V MAPE 
+###################### 
 
 mkdir 1plots 
 mkdir 2sequences
